bac56b484dc2f1f8752515167b1dc8210286c32b
Christophe Boucaut <boucaut.christophe@gmail.com>
2016-01-01 12:03:34 +0100

    Init the load the main module and a example to load another module.

---
+++ app/scripts/app.js
@@ -0,0 +1,6 @@
+import controllersModule, { myFuncError } from 'scripts/controllers';
+
+console.log('Where my app is init');
+console.log('Controller module : ');
+console.log(controllersModule);
+console.log(myFuncError);
---
+++ app/scripts/controllers.js
@@ -0,0 +1,5 @@
+export function myFuncError() {
+
+}
+
+export default 'MyModule';
