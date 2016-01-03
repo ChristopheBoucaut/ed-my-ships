8968480c0af8af1dcff9f2a7bf04b23544cc9334
Christophe Boucaut <boucaut.christophe@gmail.com>
2016-01-03 13:17:49 +0100

    Add translation service.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,22 +7,56 @@ var mainModule = angular.module(nameModule, [
     controllersModule,
     servicesModule,
     'ngRoute',
+    'ngSanitize',
     'ngMaterial',
+    'pascalprecht.translate',
     'cbAngular.utils.storage'
 ]);
 
+// Config router.
 mainModule.config([
     '$routeProvider',
     function ($routeProvider) {
         $routeProvider
             .when(
-                "/",
+                '/',
                 {
-                    controller: "HomepageController",
-                    templateUrl: "homepage.html"
+                    controller: 'HomepageController',
+                    templateUrl: 'homepage.html'
                 }
             )
-            .otherwise({ redirectTo: "/" });
+            .otherwise({ redirectTo: '/' });
+    }
+]);
+
+// config translate module.
+mainModule.config([
+    '$translateProvider',
+    function($translateProvider) {
+        $translateProvider.useStaticFilesLoader({
+            prefix: 'translation.',
+            suffix: '.json'
+        });
+
+        $translateProvider.determinePreferredLanguage(determinePreferredLanguage);
+        $translateProvider.useSanitizeValueStrategy('sanitize');
+
+        /**
+         * Determine the preferred language for the user.
+         *
+         * @return string Return the language.
+         */
+        function determinePreferredLanguage() {
+            var listLang = new Array('fr');
+
+            var currentLang = navigator.language.substr(0,2);
+
+            if (listLang.indexOf(currentLang) > -1) {
+                return currentLang;
+            } else {
+                return 'fr';
+            }
+        }
     }
 ]);
 
